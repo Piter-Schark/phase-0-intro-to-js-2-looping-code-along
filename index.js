fe334f2b55b8f034450325b8f4036e85401611ad
Peter <pschoenbauer@gmail.com>
2023-03-04 21:17:47 -0500

    commit tree 7d5c6a86ff59397a1efaa15741ca536b010a3e40

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+//
+//function wrapGifts(gifts) {
+//    for (let i = 0; i < gifts.length; i++){
+//        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//    }
+//
+//    return gifts;
+//}
+
+//wrapGifts(gifts);
+
+// new code break
+
+
+const names = ["Ellen", "Joe", "Billy"];
+
+function writeCards(names, event) {
+    const cards = [];
+    for (let i=0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+writeCards(cards, "anniversary");
+
+function countDown() {
+    let number = 10;
+    while (number > -1) {
+        console.log(number--);
+    }
+}
+
